690c0a6f2848c8740a6ce84415ea1ec80d59535a
c.misman <mismancelaleddin@gmail.com>
2023-05-31 19:13:49 +0300

    jms updates

--- src/main/java/com/ms/msjms/MsJmsApplication.java
+++ src/main/java/com/ms/msjms/MsJmsApplication.java
@@ -7,16 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MsJmsApplication {
 
     public static void main(String[] args) {
-        /*
-        ActiveMQServer activeMQServer = ActiveMQServers.newActiveMQServer(
-                new ConfigurationImpl()
-                        .setPersistenceEnabled(true)
-                        .setJournalDirectory("target/data/journal")
-                        .setSecurityEnabled(false)
-                        .addAcceptorConfiguration("invm", "vm://0")
-        );
-        activeMQServer.start();
-         */
         SpringApplication.run(MsJmsApplication.class, args);
     }
 
